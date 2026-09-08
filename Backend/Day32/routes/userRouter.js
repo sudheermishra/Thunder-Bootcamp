4b32d9e699f453f7e3f3a1798147c32f76a88095
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-08 12:40:07 +0530

    aaply ratelimit to login singup profile delete logout

--- Backend/Day32/routes/userRouter.js
+++ Backend/Day32/routes/userRouter.js
@@ -7,13 +7,26 @@ import {
   deleteAccount,
 } from "../controllers/userController.js";
 import authUserMiddleware from "../middlewares/authUserMiddleware.js";
+import authenticateRateLimiter from "../middlewares/authenticatedRateLimiter.js";
+import unAuthenticateRateLimiter from "../middlewares/unAuthenticatedRateLimiter.js";
+
 const userRouter = express.Router();
 
 // api's for user
-userRouter.post("/login", login);
-userRouter.post("/signup", signup);
-userRouter.post("/logout", logout);
-userRouter.get("/profile", authUserMiddleware, profile);
-userRouter.delete("/delete", authUserMiddleware, deleteAccount);
+userRouter.post("/login", authenticateRateLimiter, login);
+userRouter.post("/signup", unAuthenticateRateLimiter, signup);
+userRouter.post("/logout", authUserMiddleware, authenticateRateLimiter, logout);
+userRouter.get(
+  "/profile",
+  authUserMiddleware,
+  authenticateRateLimiter,
+  profile,
+);
+userRouter.delete(
+  "/delete",
+  authUserMiddleware,
+  authenticateRateLimiter,
+  deleteAccount,
+);
 
 export default userRouter;
