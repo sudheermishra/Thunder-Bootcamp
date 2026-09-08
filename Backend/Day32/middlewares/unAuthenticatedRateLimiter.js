86c436b57a566c84abc2c166df920bb6218c9ba3
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-08 12:39:20 +0530

    fix

--- Backend/Day32/middlewares/unAuthenticatedRateLimiter.js
+++ Backend/Day32/middlewares/unAuthenticatedRateLimiter.js
@@ -1,19 +1,19 @@
-import { redis } from "../config/redis.js";
+import { redisClient } from "../config/redis.js";
 
 const unAuthenticateRateLimiter = async (req, resp, next) => {
   try {
     const key = `rate-limit:ip: ${req.ip}`;
-    const requestCount = await redis.incr(key);
+    const requestCount = await redisClient.incr(key);
 
     if (requestCount === 1) {
-      await redis.expire(key, 60);
+      await redisClient.expire(key, 60);
     }
 
     if (requestCount > 10) {
-      const remainingTime = await redis.ttl(key);
+      const remainingTime = await redisClient.ttl(key);
 
       return resp.status(429).json({
-        message: `Too Many Request try after${remainingTime}`,
+        message: `Too many requests. Try again after ${remainingTime} seconds.`,
       });
     }
 
