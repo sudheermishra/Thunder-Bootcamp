f37a347c8b09e4cad8ec09911ae3aa12cf508abc
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-13 15:56:47 +0530

    loaduser middleware apply

--- Backend/Day32/routes/userRouter.js
+++ Backend/Day32/routes/userRouter.js
@@ -9,23 +9,26 @@ import {
 import authUserMiddleware from "../middlewares/authUserMiddleware.js";
 import authenticateRateLimiter from "../middlewares/authenticatedRateLimiter.js";
 import unAuthenticateRateLimiter from "../middlewares/unAuthenticatedRateLimiter.js";
+import loadUserMiddleware from "../middlewares/loadUserMiddleware.js";
 
 const userRouter = express.Router();
 
 // api's for user
-userRouter.post("/login", authenticateRateLimiter, login);
+userRouter.post("/login", unAuthenticateRateLimiter, login);
 userRouter.post("/signup", unAuthenticateRateLimiter, signup);
 userRouter.post("/logout", authUserMiddleware, authenticateRateLimiter, logout);
 userRouter.get(
   "/profile",
   authUserMiddleware,
   authenticateRateLimiter,
+  loadUserMiddleware,
   profile,
 );
 userRouter.delete(
   "/delete",
   authUserMiddleware,
   authenticateRateLimiter,
+  loadUserMiddleware,
   deleteAccount,
 );
 
