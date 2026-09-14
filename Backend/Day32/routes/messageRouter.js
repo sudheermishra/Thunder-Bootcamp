eb2698f73851471b97b49947729a5fbe364dc99f
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-14 17:51:28 +0530

    fix

--- Backend/Day32/routes/messageRouter.js
+++ Backend/Day32/routes/messageRouter.js
@@ -1,11 +1,22 @@
 import express from "express";
 import authUserMiddleware from "../middlewares/authUserMiddleware.js";
 import { sendMessage, getMessage } from "../controllers/messageController.js";
+import authenticateRateLimiter from "../middlewares/authenticatedRateLimiter.js";
+import loadUserMiddleware from "../middlewares/loadUserMiddleware.js";
+import tokenUsedMiddleware from "../middlewares/tokenUsedMiddleware.js";
+
 const messageRouter = express.Router();
 
 messageRouter.use(authUserMiddleware);
-messageRouter.post("/", sendMessage);
-messageRouter.get("/:chatId", getMessage);
-messageRouter.post("/:chatId", sendMessage);
+messageRouter.use(authenticateRateLimiter);
+
+messageRouter.post("/", tokenUsedMiddleware, loadUserMiddleware, sendMessage);
+messageRouter.get("/:chatId", loadUserMiddleware, getMessage);
+messageRouter.post(
+  "/:chatId",
+  tokenUsedMiddleware,
+  loadUserMiddleware,
+  sendMessage,
+);
 
 export default messageRouter;
