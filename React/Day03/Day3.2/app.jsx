ea19f33cb06a77ab531d7a56dcdb7501fec746f2
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-15 11:57:04 +0530

    react using module react react-dom vite bundler

---
+++ React/Day03/Day3.2/app.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Header() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2> Welcome to world </h2>
+    </>
+  );
+}
+
+function Main() {
+  return (
+    <>
+      <p>Here we are going to talk about strike what course we offer</p>
+      <ul>
+        <li>Web Devlopment</li>
+        <li>DSA</li>
+        <li>GenAI</li>
+        <li>Deep Learning</li>
+        <li>Devops</li>
+      </ul>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <>
+      <h3>I am footer of Strike</h3>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      {/* {Header()}
+      {Main()}
+      {Footer()} */}
+
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
