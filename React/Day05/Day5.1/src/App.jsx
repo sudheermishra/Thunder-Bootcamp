d7333b23f5a765ebbf5f32c6eb7591f7234053be
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-18 17:13:49 +0530

    setInterval in useEffect

--- React/Day05/Day5.1/src/App.jsx
+++ React/Day05/Day5.1/src/App.jsx
@@ -2,17 +2,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [show, setShow] = useState(true);
 
+  // jab phli baar useEeffect chalega toh toh callback fucntion ko nhi chalayega woh usko yaad krke rakhega
+  // jab second time state change hogi tab woh phle return function ko excute krega
+  // and fir upper se useeffect run hoga and wapis se setInterval nayi id na bana de isliye wahi se return kra diya
   useEffect(() => {
-    setInterval(() => {
+    if (!show) {
+      return;
+    }
+    const id = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
       console.log("hllo");
     }, 1000);
-  }, []);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [show]);
 
   return (
     <>
-      <h1>Current Time:{time}</h1>
+      <button onClick={() => setShow(!show)}>{show ? "show" : "hide"}</button>
+      {show && <h1>Current Time:{time}</h1>}
     </>
   );
 }
