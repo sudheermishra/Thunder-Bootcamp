a6acc3aa98a9197505cbb31a9f16d41e3a875b7a
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-22 14:41:12 +0530

    useRef use in form

--- React/Day07/src/Forms.jsx
+++ React/Day07/src/Forms.jsx
@@ -1,3 +1,6 @@
+// on every key stroke  yeh function render / component render hoga so we can use useRef
+// sir initial time p create hota h as a object leta and object ki value change hone p render nhi hota
+
 // import { useState } from "react";
 
 // function Forms() {
@@ -41,3 +44,35 @@
 // }
 
 // export default Forms;
+
+import { useState, useRef } from "react";
+
+function Forms() {
+  const nameRef = useRef(null);
+  const ageRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  console.log("Render");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(ageRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(passwordRef.current.value);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={nameRef}></input>
+        <input type="number" ref={ageRef}></input>
+        <input type="email" ref={emailRef}></input>
+        <input type="password" ref={passwordRef}></input>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default Forms;
