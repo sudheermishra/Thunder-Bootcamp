dd68c737508dea68b3aacff15932d7074fc9f408
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-22 14:49:42 +0530

    fix

--- React/Day07/src/main.jsx
+++ React/Day07/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 
 // import App from "./App.jsx";
 // import Stopwatch from "./Stopwatch.jsx";
-import Forms from "./Forms.jsx";
+// import Forms from "./Forms.jsx";
+import Reactform from "./Reactform.jsx";
 
-createRoot(document.getElementById("root")).render(<Forms />);
+createRoot(document.getElementById("root")).render(<Reactform />);
