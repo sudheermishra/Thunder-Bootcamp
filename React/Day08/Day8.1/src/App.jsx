ef46a7bdb1dbff74922314a7a1c0be02b88e93a3
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-24 14:04:22 +0530

    createContext

--- React/Day08/Day8.1/src/App.jsx
+++ React/Day08/Day8.1/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import Counter from "./Counter.jsx";
 import Header from "./Header.jsx";
+import { createContext } from "react";
 
+// global state le create context me
+// provider bana diya
+// jha consume krna h wha useContext hook se consume kr lenge
+export const CountContext = createContext();
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <Header count={count} />
-      <Counter count={count} setCount={setCount} />
+      <CountContext value={{ count, setCount }}>
+        <h1>I'm the App</h1>
+        <Header />
+        <Counter />
+      </CountContext>
     </>
   );
 }
