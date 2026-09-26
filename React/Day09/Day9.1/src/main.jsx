1917acc730c3a27166515eb8b00282848f077d2c
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-26 18:04:29 +0530

    fix

--- React/Day09/Day9.1/src/main.jsx
+++ React/Day09/Day9.1/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
+import Cart from "./Cart";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Cart />);
